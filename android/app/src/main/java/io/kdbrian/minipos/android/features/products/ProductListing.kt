5ec0029fe574457a1c279c1182fca3a5f9f37736
kidiga <juniorprogrammer09@gmail.com>
2025-01-16 15:42:13 +0300

    starting with the product screen

---
+++ android/app/src/main/java/io/kdbrian/minipos/android/features/products/ProductListing.kt
@@ -0,0 +1,10 @@
+package io.kdbrian.minipos.android.features.products
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ProductListing(
+    modifier: Modifier  = Modifier
+){
+}
