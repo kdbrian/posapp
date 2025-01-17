77a6a25358b6259baeae13e76b3907090db9f96e
kidiga <juniorprogrammer09@gmail.com>
2025-01-17 15:19:41 +0300

    the graphql script for stores, declaration of Jpa helper methods in the store dao and adjustment in the store service abstraction layer

--- backend/src/main/java/io/apptales/minipos/data/dao/StoreDao.java
+++ backend/src/main/java/io/apptales/minipos/data/dao/StoreDao.java
@@ -4,6 +4,14 @@ import io.apptales.minipos.data.model.Store;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StoreDao extends MongoRepository<Store, String> {
+    List<Store> findByStoreOwner(String owner);
+
+    List<Store> findByStoreName(String name);
+
+    List<Store> findByStoreLocation(String location);
+
 }
--- backend/src/main/java/io/apptales/minipos/service/StoreService.java
+++ backend/src/main/java/io/apptales/minipos/service/StoreService.java
@@ -1,11 +1,25 @@
 package io.apptales.minipos.service;
 
 import io.apptales.minipos.data.model.Store;
+import io.apptales.minipos.domain.dto.StoreDto;
 
 import java.util.List;
 
 public interface StoreService {
+
     List<Store> getAllStores();
-    Store addStore();
+
+    Store getStoreById(String storeId);
+
+    List<Store> getStoreByName(String storeName);
+
+    List<Store> getStoreByLocation(String locationName);
+
+    Store addStore(StoreDto dto);
+
+    Store updateStore(StoreDto dto);
+
+    void deleteStore(String storeId);
+
 
 }
